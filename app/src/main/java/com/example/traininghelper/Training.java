62191900a76818c26cbda686ef6182a94668047c
Artu211 <160549068+Artu211@users.noreply.github.com>
2026-01-12 18:46:05 +0100

    Update Training.java

--- app/src/main/java/com/example/traininghelper/Training.java
+++ app/src/main/java/com/example/traininghelper/Training.java
@@ -1,23 +1,14 @@
 package com.example.traininghelper;
 
 public class Training {
-    private int id;
     private String name;
     private int reps;
-    private int duration;
-    private String date;
-    private String difficulty;
 
-    public Training(int id, String name, int reps, int duration, String date, String difficulty) {
-        this.id = id;
+    public Training(String name, int reps) {
         this.name = name;
         this.reps = reps;
-        this.duration = duration;
-        this.date = date;
-        this.difficulty = difficulty;
     }
 
     public String getName() { return name; }
     public int getReps() { return reps; }
-    public String getDate() { return date; }
 }
